15b72b22bfc8a4f54081eec65936759f533318d4
“Anoop <srivanoo21@gmail.com>
2021-10-16 07:16:54 +0530

    added a save model function

--- src/utils/model.py
+++ src/utils/model.py
@@ -17,3 +17,14 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
 
 
     return model_clf ## untrained model
+
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
+
